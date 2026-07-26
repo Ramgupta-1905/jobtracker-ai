42c923074fe6d55125bf1d4533012bc0ef5aec6b
Ram Gupta <ramgupta1906@gmail.com>
2026-07-26 16:29:11 +0530

    commit

---
+++ frontend/src/components/dashboard/Sidebar.jsx
@@ -0,0 +1,95 @@
+import {
+  LayoutDashboard,
+  Briefcase,
+  FileText,
+  Calendar,
+  BarChart3,
+  Settings,
+  LogOut,
+  icons,
+  CircleUser,
+} from "lucide-react";
+
+const navItems = [
+  {
+    title: "Dashboard",
+    icon: LayoutDashboard,
+    active: true,
+  },
+  {
+    title: "Applications",
+    icon: Briefcase,
+    active: false,
+  },
+  {
+    title: "Resume Vault",
+    icon: FileText,
+    active: false,
+  },
+  {
+    title: "Interviews",
+    icon: Calendar,
+    active: false,
+  },
+  {
+    title: "Analytics",
+    icon: BarChart3,
+    active: false,
+  },
+  {
+    title: "Settings",
+    icon: Settings,
+    active: false,
+  },
+  {
+    title :"Profile",
+    icon : CircleUser,
+    active :false,
+  },
+  {
+    title: "Logout",
+    icon: LogOut,
+    active: false,
+  },
+];
+
+export default function Sidebar() {
+  return (
+    <div className="w-64 min-h-screen bg-gray-900 text-white border-r border-gray-800">
+      {/* Branding */}
+      <div className="flex items-center gap-3 p-6 border-b border-gray-800">
+        <div className="w-11 h-11 rounded-xl bg-blue-600 flex items-center justify-center text-xl font-bold">
+          J
+        </div>
+
+        <div>
+          <h1 className="text-lg font-semibold">JobTrack AI</h1>
+          <p className="text-sm text-gray-400">
+            AI Career Tracker
+          </p>
+        </div>
+      </div>
+
+      {/* Navigation */}
+      <div className="p-4">
+        {navItems.map((item) => {
+          const Icon = item.icon;
+
+          return (
+            <div
+              key={item.title}
+              className={`flex items-center gap-3 px-4 py-3 rounded-lg cursor-pointer mt-2 transition-colors ${
+                item.active
+                  ? "bg-blue-600 text-white"
+                  : "text-gray-300 hover:bg-gray-800 hover:text-white"
+              }`}
+            >
+              <Icon size={20} />
+              <span>{item.title}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,23 @@
+import Sidebar from "../components/dashboard/Sidebar";
+
 function Dashboard() {
   return (
-    <h1 className="text-white text-5xl">
-      Dashboard
-    </h1>
+    <div className="flex">
+      <Sidebar />
+
+      <div className="flex-1 bg-gray-950 text-white p-8">
+        <div>
+        <h1 className="text-3xl font-bold">
+          Good Afternoon, Alex 👋
+        </h1>
+
+        <p className="text-gray-400 mt-2">
+            Keep applying.
+            You're closer than you think.
+        </p>
+      </div>
+      </div>
+    </div>
   );
 }
 
